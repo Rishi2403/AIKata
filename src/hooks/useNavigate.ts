3253050f33c1ae1a96d0b88f7696fe271333b236
Rishi Bhattasali <rishibhattasali@gmail.com>
2025-12-15 18:52:24 +0530

    Remove redundant files

--- src/hooks/useNavigate.ts
+++
@@ -1,8 +0,0 @@
-import { useContext } from 'react'
-import { NavigationContext } from '../contexts/NavigationContext'
-
-export const useNavigate = () => {
-  const context = useContext(NavigationContext)
-  if (!context) throw new Error('useNavigate must be used within NavigationProvider')
-  return context
-}
